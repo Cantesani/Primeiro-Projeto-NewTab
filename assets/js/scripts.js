676b0a9f73c3d6aa50e7e85d43849b2162fe7632
Gabriel Cantesani <cantesani@hotmail.com>
2022-02-07 22:19:55 -0300

    Inicio do JS no projeto

---
+++ assets/js/scripts.js
@@ -0,0 +1,35 @@
+pessoasCru = localStorage.getItem('pessoas')
+if (pessoasCru != null) {
+    var pessoas = JSON.parse(pessoasCru)
+} else {
+    var pessoas = [];
+}
+
+function desenhaTabela() {
+
+    linhasExistentes = [...document.querySelectorAll('table.lista tbody .conteudo-dinamico')];
+
+    linhasExistentes.forEach(element => {
+        element.remove()
+    });
+
+    for (pessoa in pessoas) {
+        document.querySelector('table.lista tbody').innerHTML +=
+            `<tr class="conteudo-dinamico" style="background-color: ${((pessoa % 2 == 0) ? '#fff' : '#eee')}">
+                <td>${pessoas[pessoa].name}</td>
+                <td>${pessoas[pessoa].tel}</td>
+                <td>${pessoas[pessoa].xp ? '<strong style="color:green"> Sim </strong>' : '<strong style="color:red"> Não </strong>'}</td>
+                <td>
+                    <button onclick="excluirUsuario(${pessoa})"> Excluir </button>
+                </td>
+            </tr>`
+    }
+}
+
+function excluirUsuario(p) {
+    pessoas.splice(p, 1);
+    desenhaTabela();
+    localStorage.setItem('pessoas', JSON.stringify(pessoas))
+}
+
+desenhaTabela()
